e474ae46d28cb8f2b9d5b22ed88c60b0f76752e3
Neville <neville98albert@gmail.com>
2022-06-02 15:49:22 -0700

    #4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+#include <stdio.h>
+
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
