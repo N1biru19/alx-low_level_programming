c79e25ce30cd69fe0fe7f8d00f450f0219eb72c5
Neville <neville98albert@gmail.com>
2022-06-02 15:38:52 -0700

    #4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,2 @@
-#include <stdio.h>
-int main{void}
-{
-	puts{*\"Programming is like building a multilingual puzzle"}
-	return (0);
-}	
+#!/bin/bash
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
